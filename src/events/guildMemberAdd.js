c26493f52661884d855e13a7d60e856e33c234ae
Kaleb Schuster <kaleb.schuster@gmail.com>
2020-11-10 12:07:34 -0500

    Random colors - sort of
    Allows for random colors if desired

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -2,6 +2,29 @@ module.exports = async (client, member) => {
 	// Log event
 	client.logger.info(`${member.guild.name}: ${member.user.tag} has joined the server.`);
 
+	// Assign automatic role
+	const autoRoleId = client.autoRoleId;
+	const autoRole = member.guild.roles.cache.get(autoRoleId);
+	if (autoRole) {
+		try {
+			await member.roles.add(autoRole);
+		} catch (err) {
+			client.logger.error(`${member.guild.name}: Unable to assign the automatic role, please ensure I have the Manage Roles permission.`);
+		}
+	}
+
+	// Assign random color
+	const colors = member.guild.roles.cache.filter(c => c.name.startsWith('#')).array();
+
+	if (colors.length > 0) {
+		const color = colors[Math.floor(Math.random() * colors.length)]; // Get color
+		try {
+			await member.roles.add(color);
+		} catch (err) {
+			client.logger.error(`${member.guild.name}: Unable to assign the automatic color, please ensure I have the Manage Roles permission.`);
+		}
+	}
+
 	// Update users table
 	client.db.users.insertRow.run(
 		member.id,
